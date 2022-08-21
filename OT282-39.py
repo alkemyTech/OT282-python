e7bbd2db88977f2490ec5ae87d92bd87ee562ca1
augusto <amonza621@gmail.com>
2022-08-21 15:51:44 -0300

    OT282-39

--- OT282-39.py
+++ OT282-39.py
@@ -4,5 +4,3 @@ logging.basicConfig(filename='app.log',
                     format='%(asctime)s - %(levelname)s - %(message)s',
                     datefmt='%Y-%m-%d',
                     level=logging.DEBUG)
-logging.info('inicio de descarga')
-logging.info('fin de descarga')
