8e1437467a052733b70b63bc436ca76cfc9ecb9d
augusto <amonza621@gmail.com>
2022-08-26 20:17:31 -0300

    OT282-47

--- OT282-23.py
+++ OT282-23.py
@@ -1,11 +1,36 @@
 from datetime import datetime, timedelta
 from textwrap import dedent
-
 from airflow import DAG
-from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from airflow.operators.dummy import DummyOperator
+from sqlalchemy import create_engine
+import pandas as pd
+import logging
 
+logging.basicConfig(filename='app.log',
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    datefmt='%Y-%m-%d',
+                    level=logging.DEBUG)
+
+#Task 1
+'''Lo ideal es modularizar las funciones para tener un código más legible permitiendo un fácil mantenimiento de este.
+No se realizó debido a que airflow, ejecutado localmente, no me permitió importar. '''
+def extract():
+    try:
+        querys = {'jujuy': 'OT282-15-tabla jujuy.sql',
+                  'palermo': 'OT282-15-tabla palermo.sql'
+                  }
+        engine = create_engine('postgresql+psycopg2://alkymer2:Alkemy23@training-main.cghe7e6sfljt.us-east-1.rds.amazonaws.com:5432/training')
+
+        for keys,values in querys.items():
+            with open (values, encoding='utf-8') as query:
+                query_ = query.read()
+            universidad = pd.read_sql_query(query_, engine)
+            name = f'universidad_{keys}.csv'
+            universidad.to_csv(name)
+    except:
+        logging.error('Error de conexión')
+        
 # Definición de DAG
 with DAG(
     dag_id = 'universidades_C',
@@ -22,27 +47,22 @@ with DAG(
     start_date=datetime(2022, 8, 21),
     catchup=False,
 ) as dag:
-    t1 = DummyOperator(
-        task_id = 'query_SQL_jujuy_utn'
+    t1 = PythonOperator(
+        task_id = 'query_universidad_c',
+        python_callable=extract
         )
     t2 = DummyOperator(
-        task_id = 'query_SQL_palermo' 
-    )
-    t3 = DummyOperator(
         task_id = 'carga_de_datos'
     )
     
     t1.doc_md = dedent(
         """\ ### Función
-        la tarea realiza una consulta a la tabla jujuy_utn obteniendo las columnas necesarias y procesamiento de datos"""
+        la tarea realiza una consulta a la tablas de Universidad_C obteniendo las columnas necesarias y procesamiento de datos"""
     )
+    
     t2.doc_md = dedent(
-        """\ ### Función
-        la tarea realiza una consulta a la tabla palermo_tres_de_febrero obteniendo las columnas necesarias y procesamiento de datos"""
-    )
-    t3.doc_md = dedent(
         """\ ### Función
         la tarea realiza la carga de datos en S3"""
     )
 # Configuración de dependencia
-    [t1, t2] >> t3
+t1 >> t2
